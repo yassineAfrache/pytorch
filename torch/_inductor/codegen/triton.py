2b8980a179f478ea22b669a47b797b866769e6a5
lezcano <lezcano-93@hotmail.com>
2024-05-22 13:52:17 +0000

    Fix mask propagation in the presence of where
    Before, when calling ops.where, masks were not properly propagated. We
    now restrict the optimisation to `ops.masked`, which I think it was what
    the original code intended to do.
    
    I'm not 100% sure that even in the masked case this code is not
    introducing some bugs, but this is a strict improvement over the
    previous state.
    
    ghstack-source-id: bf1df215a400d8578023dfc0dff2ae28f961d602
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/125574

--- torch/_inductor/codegen/triton.py
+++ torch/_inductor/codegen/triton.py
@@ -408,12 +408,6 @@ class TritonCSEVariable(CSEVariable):
         self.mask_vars: Set[str] = set()
 
     def update_on_args(self, name, args, kwargs):
-        # When making a variable that is going to be used in indirect indexing
-        # if a where clause is used it should mean that the result is always a
-        # valid index, so you shouldn't include any of the dependent variables
-        # in the resulting load mask
-        if name == "where":
-            return
         for arg in args:
             if isinstance(arg, TritonCSEVariable):
                 self.mask_vars.update(arg.mask_vars)
@@ -869,7 +863,9 @@ class TritonKernelOverrides(TritonOverrides):
             f"tl.full({result}.shape, {triton_constant(other)}, {result}.dtype)",
             bounds=ValueRanges.wrap(other),
         )
-        return ops.where(new_mask, result, other)
+        ret = ops.where(new_mask, result, other)
+        ret.mask_vars.discard(new_mask)
+        return ret
 
     @staticmethod
     def load_seed(name, offset):
