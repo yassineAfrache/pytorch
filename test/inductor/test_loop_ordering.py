4eecfe993ba010475b4b6b6c2c5fe17b2246873a
Shunting Zhang <shunting@fb.com>
2024-05-24 17:51:08 -0700

    [wip][inductor] move loop ordering after fusion
    ghstack-source-id: 71a7e8d982549cbe100a641bd71f6f3cb504193f
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/126254

---
+++ test/inductor/test_loop_ordering.py
@@ -0,0 +1,113 @@
+# Owner(s): ["module: inductor"]
+import contextlib
+
+import torch
+from torch._inductor import config as inductor_config, ir, metrics
+from torch._inductor.codegen.triton import TritonScheduling
+from torch._inductor.graph import GraphLowering
+from torch._inductor.scheduler import SchedulerNode
+from torch._inductor.test_case import run_tests, TestCase
+from torch._inductor.test_operators import realize
+from torch._inductor.virtualized import V
+from torch.testing._internal.inductor_utils import HAS_CUDA
+
+
+class MockScheduler:
+    available_buffer_names = ()
+
+    @staticmethod
+    def get_backend(cls, *args):
+        return TritonScheduling(cls)
+
+
+class ImplDetailTest(TestCase):
+    _exit_stack = None
+
+    @staticmethod
+    def _create_computed_buffer():
+        box_a = ir.TensorBox.create(
+            ir.Buffer(
+                "a",
+                ir.FixedLayout(torch.device("cuda"), torch.float32, [32, 64], [64, 1]),
+            )
+        )
+        box_a_loader = box_a.make_loader()
+
+        def inner_fn(index):
+            return box_a_loader(index) * 2
+
+        buf = ir.Pointwise.create(
+            device=box_a.get_device(),
+            dtype=box_a.get_dtype(),
+            inner_fn=inner_fn,
+            ranges=box_a.get_size(),
+        )
+        buf.realize()
+        return buf.data.data
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+        gm = torch.fx.symbolic_trace(lambda: 0)
+        graph = GraphLowering(gm)
+        graph.scheduler = MockScheduler
+        cls._exit_stack = contextlib.ExitStack()
+        cls._exit_stack.enter_context(V.set_graph_handler(graph))
+
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+        cls._exit_stack.close()
+
+    def test_reorder_twice(self):
+        """
+        This may happen in practice if we pick a order when fusing A and B.
+        Then we pick another order for AB when we fusion C into it.
+
+        E.g. happens for BertForMaskedLM.
+        """
+        buf = self._create_computed_buffer()
+        snode = SchedulerNode(V.graph.scheduler, buf)
+        snode.apply_new_loop_order([1, 0])
+        snode.apply_new_loop_order([1, 0])
+
+        print(snode)
+        # breakpoint()
+        # TODO: this test fail so far.
+        # Add some assertions
+
+
+@inductor_config.patch(
+    {
+        "benchmark_kernel": True,
+        "triton.unique_kernel_names": True,
+    }
+)
+class LoopOrderingTest(TestCase):
+    def test_apbt_realize(self):
+        M = 1024
+        N = 2048
+
+        def f(x, y):
+            """
+            There will be 2 kernels being generated without loop ordering after fusion:
+              https://gist.github.com/shunting314/44df83f71de2c110232c50ac6638ed69
+            """
+            x = realize(x * 2)
+            y = realize(y * 3)
+            return x + y
+
+        x = torch.randn(M, N)
+        y = torch.randn(N, M).t()
+
+        expect = f(x, y)
+        actual = torch.compile(f)(x, y)
+        self.assertTrue(torch.allclose(expect, actual, atol=1e-3, rtol=1e-3))
+        self.assertEqual(1, metrics.generated_kernel_count)
+
+
+if __name__ == "__main__":
+    if HAS_CUDA:
+        torch.set_default_device("cuda")
+        run_tests()
